75287c68e52d6a634ad3213461a82dd57aabadc3
Arlo Armstrong <arlo.phoenix@gmail.com>
2018-03-05 22:45:07 -0800

    updates seed data

--- seeds/data-sample.js
+++ seeds/data-sample.js
@@ -8,6 +8,30 @@
 /* eslint-disable no-restricted-syntax, no-await-in-loop */
 
 const faker = require('faker');
+const fs = require('fs');
+const readline = require('readline');
+
+async function readLines({ path }) {
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream(path),
+  });
+
+  let onClose;
+  const promise = new Promise(resolve => {
+    onClose = resolve;
+  });
+
+  const output = [];
+  lineReader.on('line', line => {
+    const [lex, word] = line.split(': ');
+    output.push({ lex, word });
+  });
+  lineReader.on('close', () => {
+    onClose(output);
+  });
+
+  return promise;
+}
 
 module.exports.seed = async db => {
   // Create 10 random website users (as an example)
@@ -41,62 +65,75 @@ module.exports.seed = async db => {
     ),
   );
 
-  // Create 500 stories
-  const stories = Array.from({ length: 500 }).map(() =>
-    Object.assign(
-      {
-        author_id: users[faker.random.number({ min: 0, max: users.length - 1 })].id,
-        title: faker.lorem
-          .sentence(faker.random.number({ min: 4, max: 7 }))
-          .slice(0, -1)
-          .substr(0, 80),
-      },
-      Math.random() > 0.3 ? { text: faker.lorem.text() } : { url: faker.internet.url() },
-      (date => ({ created_at: date, updated_at: date }))(faker.date.past()),
-    ),
-  );
+  // // Create 500 stories
+  // const stories = Array.from({ length: 500 }).map(() =>
+  //   Object.assign(
+  //     {
+  //       author_id: users[faker.random.number({ min: 0, max: users.length - 1 })].id,
+  //       title: faker.lorem
+  //         .sentence(faker.random.number({ min: 4, max: 7 }))
+  //         .slice(0, -1)
+  //         .substr(0, 80),
+  //     },
+  //     Math.random() > 0.3 ? { text: faker.lorem.text() } : { url: faker.internet.url() },
+  //     (date => ({ created_at: date, updated_at: date }))(faker.date.past()),
+  //   ),
+  // );
+  //
+  // await Promise.all(
+  //   stories.map(story =>
+  //     db
+  //       .table('stories')
+  //       .insert(story)
+  //       .returning('id')
+  //       .then(rows =>
+  //         db
+  //           .table('stories')
+  //           .where('id', '=', rows[0])
+  //           .first(),
+  //       )
+  //       .then(row => Object.assign(story, row)),
+  //   ),
+  // );
+  //
+  // // Create some user comments
+  // const comments = Array.from({ length: 2000 }).map(() =>
+  //   Object.assign(
+  //     {
+  //       story_id: stories[faker.random.number({ min: 0, max: stories.length - 1 })].id,
+  //       author_id: users[faker.random.number({ min: 0, max: users.length - 1 })].id,
+  //       text: faker.lorem.sentences(faker.random.number({ min: 1, max: 10 })),
+  //     },
+  //     (date => ({ created_at: date, updated_at: date }))(faker.date.past()),
+  //   ),
+  // );
+  //
+  // await Promise.all(
+  //   comments.map(comment =>
+  //     db
+  //       .table('comments')
+  //       .insert(comment)
+  //       .returning('id')
+  //       .then(rows =>
+  //         db
+  //           .table('comments')
+  //           .where('id', '=', rows[0])
+  //           .first(),
+  //       )
+  //       .then(row => Object.assign(comment, row)),
+  //   ),
+  // );
 
-  await Promise.all(
-    stories.map(story =>
-      db
-        .table('stories')
-        .insert(story)
-        .returning('id')
-        .then(rows =>
-          db
-            .table('stories')
-            .where('id', '=', rows[0])
-            .first(),
-        )
-        .then(row => Object.assign(story, row)),
-    ),
-  );
+  let lexWordArray = await readLines({ path: './seeds/csw15.list' });
+  console.log(lexWordArray);
+  const chunkSize = 1280; // bind message supplies 2560 parameters; 1 param needed per column
+  while (lexWordArray.length) {
+    console.log(`words remaining: ${lexWordArray.length}`);
+    const chunk = lexWordArray.slice(0, chunkSize);
+    await db.table('corpus').insert(chunk);
+    lexWordArray = lexWordArray.slice(chunkSize);
+  }
 
-  // Create some user comments
-  const comments = Array.from({ length: 2000 }).map(() =>
-    Object.assign(
-      {
-        story_id: stories[faker.random.number({ min: 0, max: stories.length - 1 })].id,
-        author_id: users[faker.random.number({ min: 0, max: users.length - 1 })].id,
-        text: faker.lorem.sentences(faker.random.number({ min: 1, max: 10 })),
-      },
-      (date => ({ created_at: date, updated_at: date }))(faker.date.past()),
-    ),
-  );
-
-  await Promise.all(
-    comments.map(comment =>
-      db
-        .table('comments')
-        .insert(comment)
-        .returning('id')
-        .then(rows =>
-          db
-            .table('comments')
-            .where('id', '=', rows[0])
-            .first(),
-        )
-        .then(row => Object.assign(comment, row)),
-    ),
-  );
+  const corpus = await db.table('corpus').count('id');
+  console.log(corpus);
 };
